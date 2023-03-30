d1d8ae46558940b56fe3c83f6b6cbbabe4ad69b0
elias <eliasey11@gmail.com>
2023-03-30 11:22:51 +0300

    message

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Elias Ayele on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
